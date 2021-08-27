fe3b7a129f533fa23896021867527ad8971c35a6
Zihyin Hsu <jenny010328@gmail.com>
2021-08-28 00:34:25 +0800

    'update'

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,8 +1,6 @@
 
 $(document).ready(function () {
-
 // 立即預約-選擇課程
-
   $('.basicCourse').click(function(){ 
     $(this).toggleClass("chooseCourse").find('i').toggleClass("opacity-1");
     // 自己以外的card 移除 chooseCourse樣式。
@@ -27,20 +25,36 @@ $(document).ready(function () {
     $('.courseName').html('<h4 class="text-decoration-underline" style="text-underline-offset: 5px;">首次體驗課程-高階</h4>');
   })  
 
-  //首次體驗 下滑出"選擇課程階級"的效果
+//首次體驗 下滑出"選擇課程階級"的效果
 $('.juniorLevel').click(function(){
   $('.courseList').slideToggle();
 })
+
+//點填寫資料、完成預約等分頁，"選擇課程階級"會隱藏
+$('.projectBtn').click(function(){
+  $('.reserveRemind').slideDown();
+})
+
+$('.fillDataBtn').click(function(){
+  $('.reserveRemind').slideUp();
+  $('.courseList').slideUp();
+})
+
+$('.reserveBtn').click(function(){
+  $('.reserveRemind').slideUp();
+  $('.courseList').slideUp();
+})
+
 
 
 
 //loader 頁面載完就移出
 $(window).on("load", function() {
   $(".loader").remove();
-
   //頁面載完先把"選擇課程階級" 藏起來
   $(".courseList").hide();
 });
+
 
 //swiper-course
 var swiper = new Swiper(".courseSwiper", {
@@ -98,6 +112,23 @@ var swiper = new Swiper('.levelSwiper', {
   },
 
 });
+
+
+//swiper-recommend
+var swiper = new Swiper('.recommendSwiper', {
+  slidesPerView: 1.5,
+  slidesPerColumnFill: "row",
+  spaceBetween: 30,
+  breakpoints: {
+    768:{
+      slidesPerView: 3,
+    },
+    992:{
+      slidesPerView: 4.5,
+    },
+  },
+});
+
 
 // datepicker
 const elem = document.querySelector('input[name="datepicker"]');
