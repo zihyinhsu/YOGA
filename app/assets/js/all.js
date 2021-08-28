5322747747a2e66585f04efcd2655050a64ebc31
Zihyin Hsu <jenny010328@gmail.com>
2021-08-29 01:53:35 +0800

    'update'

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,34 +1,28 @@
 
 $(document).ready(function () {
-// 立即預約-選擇課程
-  $('.basicCourse').click(function(){ 
-    $(this).toggleClass("chooseCourse").find('i').toggleClass("opacity-1");
-    // 自己以外的card 移除 chooseCourse樣式。
-    $(this).parent().siblings().find('.card').removeClass('chooseCourse').find('i').removeClass("opacity-1");
-    $('.finalChoose').slideUp().slideDown();
-    $('.courseName').html('<h4 class="text-decoration-underline" style="text-underline-offset: 5px;">首次體驗課程-基礎</h4>');
-  })  
 
-  $('.intermediateCourse').click(function(){ 
-    $(this).toggleClass("chooseCourse").find('i').toggleClass("opacity-1");
-    // 自己以外的card 移除 chooseCourse樣式。
-    $(this).parent().siblings().find('.card').removeClass('chooseCourse').find('i').removeClass("opacity-1");
-    $('.finalChoose').slideUp().slideDown();
-    $('.courseName').html('<h4 class="text-decoration-underline" style="text-underline-offset: 5px;">首次體驗課程-中階</h4>');
-  })  
-
-  $('.advancedCourse').click(function(){ 
-    $(this).toggleClass("chooseCourse").find('i').toggleClass("opacity-1");
-    // 自己以外的card 移除 chooseCourse樣式。
-    $(this).parent().siblings().find('.card').removeClass('chooseCourse').find('i').removeClass("opacity-1");
-    $('.finalChoose').slideUp().slideDown();
-    $('.courseName').html('<h4 class="text-decoration-underline" style="text-underline-offset: 5px;">首次體驗課程-高階</h4>');
-  })  
+  // 立即預約-選擇課程
+$('.basicCourse,.intermediateCourse,.advancedCourse').click(function(){ 
+  $(this).toggleClass("chooseCourse").find('i').toggleClass("opacity-1");
+  // 自己以外的card 移除 chooseCourse樣式。
+  $(this).parent().siblings().find('.card').removeClass('chooseCourse').find('i').removeClass("opacity-1");
+  $('.finalChoose').slideUp().slideDown();
+}) 
+$('.basicCourse').click(function(){ 
+  $('.courseName').html('<h4 class="text-decoration-underline" style="text-underline-offset: 5px;">首次體驗課程-基礎</h4>');
+})  
+$('.intermediateCourse').click(function(){ 
+  $('.courseName').html('<h4 class="text-decoration-underline" style="text-underline-offset: 5px;">首次體驗課程-中階</h4>');
+})  
+$('.advancedCourse').click(function(){ 
+  $('.courseName').html('<h4 class="text-decoration-underline" style="text-underline-offset: 5px;">首次體驗課程-高階</h4>');
+})  
 
 //首次體驗 下滑出"選擇課程階級"的效果
-$('.juniorLevel').click(function(){
-  $('.courseList').slideToggle();
-  $('.keepReserveBtn').slideToggle();
+$('.firstTimeBtn').click(function(){
+  $('.courseList,.keepReserveBtn').slideToggle();
+  //把其他兩個卡片藏起來
+  $('.shortTimeExp,.longTimeExp').addClass('d-none').addClass('d-lg-block');
 })
 
 //點填寫資料、完成預約等分頁，"選擇課程階級"會隱藏
@@ -36,73 +30,88 @@ $('.projectBtn').click(function(){
   $('.reserveRemind').slideDown();
 })
 
-$('.fillDataBtn').click(function(){
-  $('.reserveRemind').slideUp();
-  $('.courseList').slideUp();
+$('.fillDataBtn,.reserveBtn').click(function(){
+  $('.reserveRemind,.courseList').slideUp();
 })
-
-$('.reserveBtn').click(function(){
-  $('.reserveRemind').slideUp();
-  $('.courseList').slideUp();
-})
-
 
 
 
 //loader 頁面載完就移出
 $(window).on("load", function() {
   $(".loader").remove();
-  //頁面載完先把"選擇課程階級" 藏起來
-  $(".courseList").hide();
 });
 
-
-//swiper-course
-var swiper = new Swiper(".courseSwiper", {
-  slidesPerView: 2,
-  spaceBetween: 30,
-  slidesPerColumn: 2,
-  slidesPerColumnFill: "row",
-  breakpoints: {
-  992:{
-    slidesPerView: 1.5,
-    slidesPerColumn: 1,
-  },
-}
-});
-
-//swiper-comments
-var swiper = new Swiper('.commentSwiper', {
-  slidesPerView: 1,
-  slidesPerColumn: 3,
-  slidesPerColumnFill: "row",
-  spaceBetween: 30,
-  breakpoints: {
-    768:{
-      slidesPerView: 2.2,
-      slidesPerColumn: 2,
+// <!-- swiper --> //
+//courseSwiper
+const courseSwiper = document.querySelector('.courseSwiper');
+if (courseSwiper) {
+const  swiper = new Swiper(".courseSwiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 2000,
     },
+    grid: {
+      rows: 2,    
+      fill: 'row',
+    },
+    breakpoints: {
     992:{
-      slidesPerView: 3,
-      slidesPerColumn: 2,
+      // loop: true,
+      slidesPerView: 1.5,
+      grid: {
+        rows: 1, 
+      },
     },
-  },
-  //Navigation
-  navigation: {
-    prevEl: ".swiper-prev",
-    nextEl: ".swiper-next",
-  },
+    }
+  });
+}
 
-});
+//commentSwiper
+const commentSwiper = document.querySelector('.commentSwiper');
+if (commentSwiper) {
+  const swiper = new Swiper(".commentSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    grid: {
+      rows: 3,    
+      fill: 'row',
+    },
+    breakpoints: {
+      768:{
+        slidesPerView: 2.2,
+        grid: {
+          rows: 2, 
+        },
+      },
+      992:{
+        slidesPerView: 3,
+        rows: 2, 
+      },
+    },
+    navigation: {
+      prevEl: ".swiper-prev",
+      nextEl: ".swiper-next",
+    },
+  });
+}
 
-//swiper-level
-var swiper = new Swiper('.levelSwiper', {
+//levelSwiper
+const levelSwiper = document.querySelector('.levelSwiper');
+if (levelSwiper) {
+const swiper = new Swiper(".levelSwiper", {
   slidesPerView: 1,
-  slidesPerColumnFill: "row",
   spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+  },
+  grid: {
+    fill: 'row',
+  },
   breakpoints: {
     768:{
-      slidesPerView: 1.8,
+      slidesPerView: 1.5,
+      // loop: true,
     },
     992:{
       slidesPerView: 2,
@@ -110,16 +119,22 @@ var swiper = new Swiper('.levelSwiper', {
     1200:{
       slidesPerView: 3,
     },
-  },
-
+  }
 });
+}
 
-
-//swiper-recommend
-var swiper = new Swiper('.recommendSwiper', {
+//recommendSwiper
+const recommendSwiper = document.querySelector('.recommendSwiper');
+if (recommendSwiper) {
+const swiper = new Swiper(".recommendSwiper", {
   slidesPerView: 1.5,
-  slidesPerColumnFill: "row",
   spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+  },
+  grid: {
+    fill: 'row',
+  },
   breakpoints: {
     768:{
       slidesPerView: 3,
@@ -127,18 +142,19 @@ var swiper = new Swiper('.recommendSwiper', {
     992:{
       slidesPerView: 4.5,
     },
-  },
+  }
 });
+}
 
 
-// datepicker
+
+// datepicker 日期選擇器
 const elem = document.querySelector('input[name="datepicker"]');
 const datepicker = new Datepicker(elem, {
  buttonClass: 'btn text-tertiary',
  nextArrow: '>',
  prevArrow: '<'
 }); 
-
 });
 
 
