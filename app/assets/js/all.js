55b6da27ac416ff9674d031d213f71448e49a9e2
Zihyin Hsu <jenny010328@gmail.com>
2021-09-12 12:48:13 +0800

    'update'

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
     delay: 0, // values from 0 to 3000, with step 50ms
     duration: 400, // values from 0 to 3000, with step 50ms
     easing: 'ease', // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
+    once: true, // whether animation should happen only once - while scrolling down
     mirror: false, // whether elements should animate out while scrolling past them
     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
   
@@ -141,14 +141,12 @@ const swiper = new Swiper(".levelSwiper", {
   },
   breakpoints: {
     768:{
-      slidesPerView: 1.5,
-      // loop: true,
-    },
-    992:{
       slidesPerView: 2,
+      // loop: true,
     },
     1200:{
       slidesPerView: 3,
+      spaceBetween: 20,
     },
   }
 });
